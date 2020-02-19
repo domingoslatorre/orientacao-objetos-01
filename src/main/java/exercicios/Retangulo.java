8bb33440e286e5e7d11ebf51c0d0153aaa001f76
Domingos Latorre <domingos.latorre@gmail.com>
2020-02-19 20:42:46 -0300

    Criação da classe Retangulo

---
+++ src/main/java/exercicios/Retangulo.java
@@ -0,0 +1,14 @@
+package exercicios;
+
+public class Retangulo {
+    public Double base;
+    public Double altura;
+
+    public Double calcularArea() {
+        return base * altura;
+    }
+
+    public Double calcularPerimetro() {
+        return 2 * (base + altura);
+    }
+}
