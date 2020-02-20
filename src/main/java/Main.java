df1a3be1c33772431aef032eb8a1737e1220fd09
Domingos Latorre <domingos.latorre@gmail.com>
2020-02-19 22:11:59 -0300

    Exemplo errado de teste na classe main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,39 +1,15 @@
 import exemplos.Aluno;
+import exemplos.Quadrado;
 
 public class Main {
     public static void main(String[] args) {
-        Aluno a1 = new Aluno();
-        a1.nome = "João";
-        a1.sobrenome = "da Silva";
-        a1.telefone = "11-1111111";
+        // Exemplo errado de teste
+        Quadrado q1 = new Quadrado();
+        q1.lado = 10.0;
 
-        Aluno a2 = new Aluno();
-        a2.nome = "Maria";
-        a2.sobrenome = "do Santos";
-        a2.telefone = "12-121212";
-
-        System.out.println(a1.nome);
-        System.out.println(a1.sobrenome);
-        //System.out.println(a1.nome + " " + a1.sobrenome);
-        System.out.println(a1.getNomeCompleto());
-
-        System.out.println(a2.nome);
-        System.out.println(a2.sobrenome);
-        //System.out.println(a2.nome + " " + a2.sobrenome);
-        System.out.println(a2.getNomeCompleto());
-
-
-        System.out.println(calcularAreaQuadrado(15.0));
-        System.out.println(calcularPerimetroQuadrado(15.0));
-
+        System.out.println(q1.calcularArea());
+        System.out.println(q1.calcularPerimetro());
 
     }
 
-    public static Double calcularAreaQuadrado(Double lado) {
-        return lado * lado;
-    }
-
-    public static Double calcularPerimetroQuadrado(Double lado) {
-        return 4 * lado;
-    }
 }
