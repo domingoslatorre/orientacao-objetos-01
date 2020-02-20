8f137b9a56748456772b8c3add65babb07b47784
rian santos <riansm100@gmail.com>
2020-02-20 16:22:50 -0300

    diminuição do codigo

--- src/main/java/exercicios/Circulo.java
+++ src/main/java/exercicios/Circulo.java
@@ -1,12 +1,8 @@
 package exercicios;
 
-public class circulo {
+public class Circulo {
     public double raio;
 
-    public double calcularArea(){
-        return Math.PI * Math.pow(raio, 2);
-    }
-    public double calcularPerimetro(){
-        return 2 * Math.PI * raio;
-    }
+    public double calcularArea() { return Math.PI * Math.pow(raio, 2); }
+    public double calcularPerimetro() { return 2 * Math.PI * raio; }
 }
