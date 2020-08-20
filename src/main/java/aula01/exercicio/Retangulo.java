8fc054c864c7e6c84b9e4ce3f64728a897f2f237
Henrique Shimada <shimada.henrique@gmail.com>
2020-08-20 00:02:13 -0300

    2 - Testing

--- src/main/java/aula01/exercicio/Retangulo.java
+++ src/main/java/aula01/exercicio/Retangulo.java
@@ -1,13 +1,28 @@
 package aula01.exercicio;
 
 public class Retangulo {
-    Double base, altura;
+    private Double base, altura;
 
-    public Double calcularArea(Double base, Double altura){
+    public Retangulo(){}
+
+    public Retangulo(Double base, Double altura){
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public Double calcularArea(){
         return base * altura;
     }
 
-    public Double calcularPerimetro(Double base, Double altura){
+    public Double calcularPerimetro(){
         return 2 * base + 2 * altura;
+    }
+
+    public Double getBase() {
+        return base;
+    }
+
+    public Double getAltura() {
+        return altura;
     }
 }
