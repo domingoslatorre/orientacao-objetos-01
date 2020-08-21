63b72ce3b0e8bd9eb136f755c2765a306f817bb8
Henrique Shimada <shimada.henrique@gmail.com>
2020-08-20 23:02:24 -0300

    Exercicio 3.1 - Implementar Funcionario

---
+++ src/main/java/aula01/exercicio/Funcionario.java
@@ -0,0 +1,26 @@
+package aula01.exercicio;
+
+public class Funcionario {
+
+    public String nome;
+    public String email;
+    public Integer idade;
+    public Double salario;
+
+    Funcionario(){}
+
+    Funcionario(String nome, String email, Integer idade, Double salario){
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    public String obterDados(){
+        return nome + ", " + idade + ", '" + email;
+    }
+    public void promover(Double porcentagemAumento){
+        this.salario = this.salario * (1 + porcentagemAumento/100);
+    }
+
+}
